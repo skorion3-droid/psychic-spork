5483453dab95ec22d0e084b9f694b6c524bda96a
skorion <skorion3@gmail.com>
2025-10-10 17:05:29 +0300

    resolve 02 task

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,9 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "abc", expected: "abc"},    // дополнительные тесты
+		{input: "abc3", expected: "abccc"}, // дополнительные тесты
+		{input: "a3", expected: "aaa"},     // дополнительные тесты
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -34,7 +37,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "1", "3a"} // дополнительные тесты
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
