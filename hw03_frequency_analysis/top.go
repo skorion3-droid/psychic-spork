0834a8505b8ae42ffe930404324c5c3134f1aaac
skorion <skorion3@gmail.com>
2025-10-27 05:50:01 +0300

    hw03_frequency_analysis

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,54 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"sort"
+	"strings"
+)
+
+type WordFr struct {
+	Word string
+	Fr   int
+}
+
+func Top10(text string) []string {
+	if strings.TrimSpace(text) == "" {
+		return []string{}
+	}
 	// Place your code here.
-	return nil
+	words := strings.Fields(text)
+	mapa := make(map[string]int)
+
+	for _, word := range words {
+		value, exists := mapa[word]
+
+		if exists {
+			mapa[word] = value + 1
+		} else {
+			mapa[word] = 1
+		}
+	}
+
+	preResult := make([]WordFr, 0)
+
+	for word, count := range mapa {
+		preResult = append(preResult, WordFr{word, count})
+	}
+
+	sort.Slice(preResult, func(i, j int) bool {
+		if preResult[i].Fr == preResult[j].Fr {
+			return preResult[i].Word < preResult[j].Word
+		}
+		return preResult[i].Fr > preResult[j].Fr
+	})
+
+	result := make([]string, 0)
+
+	for i, wordFrequency := range preResult {
+		if i == 10 {
+			break
+		}
+		result = append(result, wordFrequency.Word)
+	}
+
+	return result
 }
